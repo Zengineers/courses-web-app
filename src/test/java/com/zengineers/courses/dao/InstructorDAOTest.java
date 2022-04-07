1bf459cc328d7a7216f040c65b3407be12290e49
ch-ant <ant.chr@outlook.com>
2022-04-07 22:58:52 +0300

    Add browse add remove update tests
    - Add CoursesControllerTest which contains tests for all the http requests performed by the method in CoursesController
    - Add CourseDAOTest
    - Add CourseServiceTest
    - Add MockObjectGenerator class responsible for generating mock object used by tests
    
    Co-Authored-By: kiko39 <79718168+kiko39@users.noreply.github.com>

--- src/test/java/com/zengineers/courses/dao/InstructorDAOTest.java
+++ src/test/java/com/zengineers/courses/dao/InstructorDAOTest.java
@@ -2,17 +2,14 @@ package com.zengineers.courses.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
 
-import com.zengineers.courses.dao.InstructorDAO;
 import com.zengineers.courses.model.Instructor;
 
 @DataJpaTest
